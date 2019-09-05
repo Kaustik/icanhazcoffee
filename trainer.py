208bf667a087caba5b2a014dc6ed0d6235a448c1
Hans Hjort <hans.n.hjort@gmail.com>
2019-09-05 17:46:36 +0200

    Trainer and data

---
+++ trainer.py
@@ -0,0 +1,7 @@
+from imageai.Detection.Custom import DetectionModelTrainer
+
+trainer = DetectionModelTrainer()
+trainer.setModelTypeAsYOLOv3()
+trainer.setDataDirectory(data_directory=".")
+trainer.setTrainConfig(object_names_array=["empty coffee", "not empty coffee"], batch_size=4, num_experiments=4, train_from_pretrained_model="pretrained-yolov3.h5")
+trainer.trainModel()
