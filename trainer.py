a1459a67dda5011e0e764e9cdc8aa338c6fb89e8
Hans Hjort <hans.n.hjort@gmail.com>
2019-09-05 20:38:24 +0200

    use execution_path

--- trainer.py
+++ trainer.py
@@ -1,7 +1,9 @@
 from imageai.Detection.Custom import DetectionModelTrainer
 
+execution_path = os.getcwd()
+
 trainer = DetectionModelTrainer()
 trainer.setModelTypeAsYOLOv3()
-trainer.setDataDirectory(data_directory=".")
+trainer.setDataDirectory(data_directory=execution_path)
 trainer.setTrainConfig(object_names_array=["empty coffee", "not empty coffee"], batch_size=4, num_experiments=4)
 trainer.trainModel()
