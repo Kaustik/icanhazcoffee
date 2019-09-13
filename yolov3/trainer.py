813c3cc822c0161ed5b896da659e13d6d426ffd1
Hans Hjort <hans.n.hjort@gmail.com>
2019-09-13 09:40:00 +0200

    Updated trainer for Kaggle run

--- yolov3/trainer.py
+++ yolov3/trainer.py
@@ -4,7 +4,8 @@ import os
 execution_path = os.getcwd()
 
 trainer = DetectionModelTrainer()
+trainer.setGpuUsage(1)
 trainer.setModelTypeAsYOLOv3()
-trainer.setDataDirectory(data_directory=execution_path)
-trainer.setTrainConfig(object_names_array=["empty coffee", "not empty coffee"], batch_size=4, num_experiments=4)
+trainer.setDataDirectory(data_directory=".")
+trainer.setTrainConfig(object_names_array=["empty coffee", "not empty coffee"], batch_size=4, num_experiments=10,train_from_pretrained_model="pretrained-yolov3.h5")
 trainer.trainModel()
